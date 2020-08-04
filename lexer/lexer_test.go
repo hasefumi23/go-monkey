682d794cdd6fb4954e2f44eadbe89ecbb4a184f5
hasefumi23 <hasefumi23@gmail.com>
2020-08-04 21:44:24 +0900

    [from now] 2020/08/04 21:44:24
    diff --git a/lexer/lexer.go b/lexer/lexer.go
    index 53474b2..a7028a0 100644
    --- a/lexer/lexer.go
    +++ b/lexer/lexer.go
    @@ -33,16 +33,28 @@ func (l *Lexer) NextToken() token.Token {
     	switch l.ch {
     	case '=':
     		tok = newToken(token.ASSIGN, l.ch)
    +	case '+':
    +		tok = newToken(token.PLUS, l.ch)
    +	case '-':
    +		tok = newToken(token.MINUS, l.ch)
    +	case '!':
    +		tok = newToken(token.BANG, l.ch)
    +	case '/':
    +		tok = newToken(token.SLASH, l.ch)
    +	case '*':
    +		tok = newToken(token.ASTERISK, l.ch)
    +	case '<':
    +		tok = newToken(token.LT, l.ch)
    +	case '>':
    +		tok = newToken(token.GT, l.ch)
     	case ';':
     		tok = newToken(token.SEMICOLON, l.ch)
    +	case ',':
    +		tok = newToken(token.COMMA, l.ch)
     	case '(':
     		tok = newToken(token.LPAREN, l.ch)
     	case ')':
     		tok = newToken(token.RPAREN, l.ch)
    -	case ',':
    -		tok = newToken(token.COMMA, l.ch)
    -	case '+':
    -		tok = newToken(token.PLUS, l.ch)
     	case '{':
     		tok = newToken(token.LBRACE, l.ch)
     	case '}':
    diff --git a/lexer/lexer_test.go b/lexer/lexer_test.go
    index 0db2286..1f3e9a3 100644
    --- a/lexer/lexer_test.go
    +++ b/lexer/lexer_test.go
    @@ -15,6 +15,8 @@ let add = fn(x, y) {
     };
    
     let result = add(five, ten);
    +!-/*5;
    +5 < 10 > 5;
     `
    
     	tests := []struct {
    @@ -30,8 +32,8 @@ let result = add(five, ten);
     		{token.IDENT, "ten"},
     		{token.ASSIGN, "="},
     		{token.INT, "10"},
    -
     		{token.SEMICOLON, ";"},
    +
     		{token.LET, "let"},
     		{token.IDENT, "add"},
     		{token.ASSIGN, "="},
    @@ -44,22 +46,35 @@ let result = add(five, ten);
     		{token.LBRACE, "{"},
     		{token.IDENT, "x"},
     		{token.PLUS, "+"},
    -
     		{token.IDENT, "y"},
     		{token.SEMICOLON, ";"},
     		{token.RBRACE, "}"},
     		{token.SEMICOLON, ";"},
    +
     		{token.LET, "let"},
     		{token.IDENT, "result"},
     		{token.ASSIGN, "="},
     		{token.IDENT, "add"},
     		{token.LPAREN, "("},
    -
     		{token.IDENT, "five"},
     		{token.COMMA, ","},
     		{token.IDENT, "ten"},
     		{token.RPAREN, ")"},
     		{token.SEMICOLON, ";"},
    +
    +		{token.BANG, "!"},
    +		{token.MINUS, "-"},
    +		{token.SLASH, "/"},
    +		{token.ASTERISK, "*"},
    +		{token.INT, "5"},
    +		{token.SEMICOLON, ";"},
    +		{token.INT, "5"},
    +		{token.LT, "<"},
    +		{token.INT, "10"},
    +		{token.GT, ">"},
    +		{token.INT, "5"},
    +		{token.SEMICOLON, ";"},
    +
     		{token.EOF, ""},
     	}
    
    diff --git a/token/token.go b/token/token.go
    index 246dabc..d97dea1 100644
    --- a/token/token.go
    +++ b/token/token.go
    @@ -16,8 +16,15 @@ const (
     	INT   = "INT"   // 134532
    
     	// 演算子
    -	ASSIGN = "="
    -	PLUS   = "+"
    +	ASSIGN   = "="
    +	PLUS     = "+"
    +	MINUS    = "-"
    +	BANG     = "!"
    +	ASTERISK = "*"
    +	SLASH    = "/"
    +
    +	LT = "<"
    +	GT = ">"
    
     	// デリミタ
     	COMMA     = ","

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -15,6 +15,8 @@ let add = fn(x, y) {
 };
 
 let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 `
 
 	tests := []struct {
@@ -30,8 +32,8 @@ let result = add(five, ten);
 		{token.IDENT, "ten"},
 		{token.ASSIGN, "="},
 		{token.INT, "10"},
-
 		{token.SEMICOLON, ";"},
+
 		{token.LET, "let"},
 		{token.IDENT, "add"},
 		{token.ASSIGN, "="},
@@ -44,22 +46,35 @@ let result = add(five, ten);
 		{token.LBRACE, "{"},
 		{token.IDENT, "x"},
 		{token.PLUS, "+"},
-
 		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
 		{token.SEMICOLON, ";"},
+
 		{token.LET, "let"},
 		{token.IDENT, "result"},
 		{token.ASSIGN, "="},
 		{token.IDENT, "add"},
 		{token.LPAREN, "("},
-
 		{token.IDENT, "five"},
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
 	}
 
