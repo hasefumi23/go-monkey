6081ecc3e5ce2d3e4a346447179540155569f949
hasefumi23 <hasefumi23@gmail.com>
2020-08-08 19:13:23 +0900

    [from now] 2020/08/08 19:13:23
    diff --git a/ast/ast.go b/ast/ast.go
    index d7cd58a..71d469c 100644
    --- a/ast/ast.go
    +++ b/ast/ast.go
    @@ -163,3 +163,12 @@ type Expression interface {
     	Node
     	expressionNode()
     }
    +
    +type Boolean struct {
    +	Token token.Token
    +	Value bool
    +}
    +
    +func (b *Boolean) expressionNode()      {}
    +func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
    +func (b *Boolean) String() string       { return b.Token.Literal }
    diff --git a/parser/parser.go b/parser/parser.go
    index ec5e68c..4f94627 100644
    --- a/parser/parser.go
    +++ b/parser/parser.go
    @@ -65,6 +65,8 @@ func New(l *lexer.Lexer) *Parser {
     	p.registerPrefix(token.INT, p.parseIntegerLiteral)
     	p.registerPrefix(token.BANG, p.parsePrefixExpression)
     	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
    +	p.registerPrefix(token.TRUE, p.parseBoolean)
    +	p.registerPrefix(token.FALSE, p.parseBoolean)
    
     	p.infixParseFns = make(map[token.TokenType]infixParseFn)
     	p.registerInfix(token.PLUS, p.parseInfixExpression)
    @@ -242,6 +244,10 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
     	return lit
     }
    
    +func (p *Parser) parseBoolean() ast.Expression {
    +	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
    +}
    +
     func (p *Parser) curTokenIs(t token.TokenType) bool {
     	return p.curToken.Type == t
     }
    diff --git a/parser/parser_test.go b/parser/parser_test.go
    index 0865735..3037168 100644
    --- a/parser/parser_test.go
    +++ b/parser/parser_test.go
    @@ -342,3 +342,97 @@ func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
    
     	return true
     }
    +
    +func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
    +	ident, ok := exp.(*ast.Identifier)
    +	if !ok {
    +		t.Errorf("exp not *ast.Identifier. got=%T", exp)
    +		return false
    +	}
    +
    +	if ident.Value != value {
    +		t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
    +		return false
    +	}
    +
    +	if ident.TokenLiteral() != value {
    +		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
    +		return false
    +	}
    +
    +	return true
    +}
    +
    +func testLiteralExpression(
    +	t *testing.T,
    +	exp ast.Expression,
    +	expected interface{},
    +) bool {
    +	switch v := expected.(type) {
    +	case int:
    +		return testIntegerLiteral(t, exp, int64(v))
    +	case int64:
    +		return testIntegerLiteral(t, exp, v)
    +	case string:
    +		return testIdentifier(t, exp, v)
    +	}
    +	t.Errorf("type of exp not handled. got=%T", exp)
    +	return false
    +}
    +
    +func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
    +	opExp, ok := exp.(*ast.InfixExpression)
    +	if !ok {
    +		t.Errorf("exp is not ast.InfixExpression. got=%T(%s)", exp, exp)
    +		return false
    +	}
    +
    +	if !testLiteralExpression(t, opExp.Left, left) {
    +		return false
    +	}
    +
    +	if opExp.Operator != operator {
    +		t.Errorf("exp.Operator is not '%s'. got=%q", operator, opExp.Operator)
    +		return false
    +	}
    +
    +	if !testLiteralExpression(t, opExp.Right, right) {
    +		return false
    +	}
    +
    +	return true
    +}
    +
    +func TestBooleanExpression(t *testing.T) {
    +	tests := []struct {
    +		input           string
    +		expectedBoolean bool
    +	}{
    +		{"true;", true},
    +		{"false;", false},
    +	}
    +
    +	for _, tt := range tests {
    +		l := lexer.New(tt.input)
    +		p := New(l)
    +		program := p.ParseProgram()
    +		checkParseErrors(t, p)
    +
    +		if len(program.Statements) != 1 {
    +			t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
    +		}
    +
    +		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
    +		if !ok {
    +			t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
    +		}
    +
    +		boolean, ok := stmt.Expression.(*ast.Boolean)
    +		if !ok {
    +			t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
    +		}
    +		if boolean.Value != tt.expectedBoolean {
    +			t.Errorf("boolean.Value not %t. got=%t", tt.expectedBoolean, boolean.Value)
    +		}
    +	}
    +}

--- ast/ast.go
+++ ast/ast.go
@@ -163,3 +163,12 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
