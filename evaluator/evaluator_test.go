ad2ca453c652a340e8e02ea6defe7269cf5070ca
hasefumi23 <hasefumi23@gmail.com>
2020-08-10 20:44:20 +0900

    [from now] 2020/08/10 20:44:20
    diff --git a/evaluator/evaluator.go b/evaluator/evaluator.go
    index f3aa532..0d8667f 100644
    --- a/evaluator/evaluator.go
    +++ b/evaluator/evaluator.go
    @@ -254,11 +254,26 @@ func evalInfixExpression(
     		return nativeBoolToBooleanObject(left != right)
     	case left.Type() != right.Type():
     		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
    +	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
    +		return evalStringInfixExpression(operator, left, right)
     	default:
     		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
     	}
     }
    
    +func evalStringInfixExpression(
    +	operator string,
    +	left, right object.Object,
    +) object.Object {
    +	if operator != "+" {
    +		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
    +	}
    +
    +	leftVal := left.(*object.String).Value
    +	rightVal := right.(*object.String).Value
    +	return &object.String{Value: leftVal + rightVal}
    +}
    +
     func evalIntegerInfixExpression(
     	operator string,
     	left, right object.Object,
    diff --git a/evaluator/evaluator_test.go b/evaluator/evaluator_test.go
    index c418105..7b16a75 100644
    --- a/evaluator/evaluator_test.go
    +++ b/evaluator/evaluator_test.go
    @@ -220,6 +220,10 @@ if (10 > 1) {
     `,
     			"unknown operator: BOOLEAN + BOOLEAN",
     		},
    +		{
    +			`"Hello" - "World"`,
    +			"unknown operator: STRING - STRING",
    +		},
     	}
    
     	for _, tt := range tests {
    @@ -317,3 +321,17 @@ func TestStringLiteral(t *testing.T) {
     		t.Errorf("String has wrong value. got=%q", str.Value)
     	}
     }
    +
    +func TestStringConcatenation(t *testing.T) {
    +	input := `"Hello" + " " + "World!"`
    +
    +	evaluated := testEval(input)
    +	str, ok := evaluated.(*object.String)
    +	if !ok {
    +		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
    +	}
    +
    +	if str.Value != "Hello World!" {
    +		t.Errorf("String has wrong value. got=%q", str.Value)
    +	}
    +}

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -220,6 +220,10 @@ if (10 > 1) {
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -306,6 +310,20 @@ func TestFunctionApplication(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
